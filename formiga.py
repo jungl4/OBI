00e8cd6e1a153438ef9da0ed5cf2967373d971b2
jungl4 <134565016+jungl4@users.noreply.github.com>
2023-06-23 16:27:24 -0300

    formiga.py

--- formiga.py
+++ formiga.py
@@ -1,11 +1,12 @@
 salões, tuneis, formiga = [int(x) for x in input().split()]
 
 prof_salões = [int(x) for x in input().split()]
-tuneis_possiveis = []
-
-for i in range(salões):
-    while i + 2 <= len(prof_salões):
-        if prof_salões[i] > prof_salões[i + 1]:
-            tuneis_possiveis.append([prof_salões[i], prof_salões[i + 1]])
-        break
+tuneis_possiveis = 0
+            
+for i in range(tuneis):
+    p1, p2 = [int(x) -1 for x in input().split()]
+    if prof_salões[p1] > prof_salões[p2] or prof_salões[p1] < prof_salões[p2] and formiga -1 == p1 or formiga -1 == p2:
+        formiga = max(p1, p2)
+        tuneis_possiveis += 1
 print(tuneis_possiveis)
+            
