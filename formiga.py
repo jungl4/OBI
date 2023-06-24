f3d7d079f940913fda80cc75deab5a3623464c12
jungl4 <134565016+jungl4@users.noreply.github.com>
2023-06-24 17:21:26 -0300

    formiga.py

--- formiga.py
+++ formiga.py
@@ -1,12 +1,29 @@
-salões, tuneis, formiga = [int(x) for x in input().split()]
+saloes, tuneis, formiga = [int(x) for x in input().split()]
+profundidade = [int(x) for x in input().split()] 
+deslizes = []
+maxv = -2000
+ações = 0
+for _ in range(tuneis):
+    deslizes += [[int(x) for x in input().split()]]
+#pegando as vars
 
-prof_salões = [int(x) for x in input().split()]
-tuneis_possiveis = 0
-            
-for i in range(tuneis):
-    p1, p2 = [int(x) -1 for x in input().split()]
-    if prof_salões[p1] > prof_salões[p2] or prof_salões[p1] < prof_salões[p2] and formiga -1 == p1 or formiga -1 == p2:
-        formiga = max(p1, p2)
-        tuneis_possiveis += 1
-print(tuneis_possiveis)
-            
+formiga -= 1
+for _ in range(tuneis):
+    tempmaxv = maxv
+    for e in range(tuneis):
+        i, j = deslizes[e]
+        i -= 1
+        j -= 1
+        if i == formiga:
+            if profundidade[i] > profundidade[j] and profundidade[j] > maxv:
+                maxv = profundidade[j]
+                tempformiga = j
+        elif j == formiga:
+            if  profundidade[j] > profundidade[i] and profundidade[i] > maxv:
+                maxv = profundidade[i]  
+                tempformiga = i
+        if tempmaxv != maxv:
+            ações += 1
+            formiga = tempformiga
+            maxv = tempmaxv
+print(ações)
